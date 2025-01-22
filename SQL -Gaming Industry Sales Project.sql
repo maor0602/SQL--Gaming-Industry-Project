1affd66a4308301c1e65569ca8d1d5d1d9306c15
Maor Barel <maor7878@gmail.com>
2025-01-22 23:10:23 +0200

    Update SQL -Gaming Industry Sales Project.sql

--- SQL -Gaming Industry Sales Project.sql
+++ SQL -Gaming Industry Sales Project.sql
@@ -1,133 +1,127 @@
 --                            SQL- Gaming Industry Sales Project
 
---QUESTION 1 A:
-with totalCount_CTE as 
-(
-select name, count (*) totalCount
-from video_games
-group by name
-having count (*)>=3 
+-- QUESTION 1 A:
+WITH totalCount_CTE AS (
+    SELECT name, COUNT(*) AS totalCount
+    FROM video_games
+    GROUP BY name
+    HAVING COUNT(*) >= 3
 )
-select count (name) totalReleased
-from totalCount_CTE
+SELECT COUNT(name) AS totalReleased
+FROM totalCount_CTE;
 
-
-
---QUESTION 1 B:
-with sumGS_CTE as
-(
-select Genre, Year_of_Release, sum(Global_Sales) sumGS
-from video_games
-group by Genre, Year_of_Release
+-- QUESTION 1 B:
+WITH sumGS_CTE AS (
+    SELECT Genre, Year_of_Release, SUM(Global_Sales) AS sumGS
+    FROM video_games
+    GROUP BY Genre, Year_of_Release
 ),
-maxGS_CTE as
-(
-select tbl1.Genre, tbl1.maxGS, tbl2.Year_of_Release Years
-from (select Genre, max(sumGS) maxGS
-from sumGS_CTE
-group by Genre) tbl1
-inner join sumGS_CTE tbl2 on tbl1.Genre= tbl2.Genre and tbl1.maxGS= tbl2.sumGS
+maxGS_CTE AS (
+    SELECT tbl1.Genre, tbl1.maxGS, tbl2.Year_of_Release AS Years
+    FROM (
+        SELECT Genre, MAX(sumGS) AS maxGS
+        FROM sumGS_CTE
+        GROUP BY Genre
+    ) tbl1
+    INNER JOIN sumGS_CTE tbl2 
+        ON tbl1.Genre = tbl2.Genre AND tbl1.maxGS = tbl2.sumGS
 )
-select top 3 Genre, Years
-from maxgs_cte
-order by count (*) over (partition by years) desc
+SELECT TOP 3 Genre, Years
+FROM maxGS_CTE
+ORDER BY COUNT(*) OVER (PARTITION BY Years) DESC;
 
+-- QUESTION 2 NORMAL AVG:
+SELECT Rating, CAST(AVG(Critic_Score) AS DECIMAL(10, 2)) AS avgCS
+FROM video_games
+GROUP BY Rating;
 
+-- QUESTION 2 WEIGHTED AVG:
+SELECT Rating,
+       CAST(SUM(Critic_Score * Critic_Count) / SUM(Critic_Count) AS DECIMAL(10, 2)) AS weightedAVG
+FROM video_games
+GROUP BY Rating;
 
---QUESTION 2 NORMAL AVG:
-select rating, cast (avg (Critic_Score) as decimal (10, 2)) avgCS
-from video_games
-group by Rating
-
-
-
---QUESTION 2 WEIGHTED AVG:
-select Rating,
-cast ((sum (Critic_Score*Critic_Count)/ sum (critic_count)) as decimal (10,2)) weightedAVG
-from video_games
-group by Rating
-
-
-
---QUESTION 2 MODE:
-with scoreCount_CTE as
-(
-select Rating, Critic_Score, count (Critic_Score) scoreCount
-from video_games
-group by Critic_Score, Rating
+-- QUESTION 2 MODE:
+WITH scoreCount_CTE AS (
+    SELECT Rating, Critic_Score, COUNT(Critic_Score) AS scoreCount
+    FROM video_games
+    GROUP BY Critic_Score, Rating
 )
-select Rating, Critic_Score as Mode
-from
-(select Rating, Critic_Score, scoreCount,
-max (scoreCount) over (partition by Rating) maxScoreCount,
-rank () over (partition by rating order by scorecount desc) rn
-from scoreCount_CTE) tbl
-where rn= 1
-order by Rating, Critic_Score
+SELECT Rating, Critic_Score AS Mode
+FROM (
+    SELECT Rating, Critic_Score, scoreCount,
+           MAX(scoreCount) OVER (PARTITION BY Rating) AS maxScoreCount,
+           RANK() OVER (PARTITION BY Rating ORDER BY scoreCount DESC) AS rn
+    FROM scoreCount_CTE
+) tbl
+WHERE rn = 1
+ORDER BY Rating, Critic_Score;
 
+-- QUESTION 2:
+-- The two ratings that have the same values for all three measures are 'K-A' AND 'AO',
+-- and this is because each of them has only one value.
 
-
---QUESTION 2:
-/*The two ratings that have the same values for all three measures are 'K-A' AND 'AO',
-and this is because each of them has only one value.*/
-
-
-
---QUESTION 3:
-with allCross_CTE as
-(select distinct vg1.Genre, vg2.Platform, vg3.Year_of_Release
-from (select distinct Genre from video_games where Genre is not null) vg1
-cross join
-(select distinct Platform from video_games where Platform is not null) vg2
-cross join
-(select distinct Year_of_Release from video_games where Year_of_Release is not null) vg3
+-- QUESTION 3:
+WITH allCross_CTE AS (
+    SELECT DISTINCT vg1.Genre, vg2.Platform, vg3.Year_of_Release
+    FROM (
+        SELECT DISTINCT Genre FROM video_games WHERE Genre IS NOT NULL
+    ) vg1
+    CROSS JOIN (
+        SELECT DISTINCT Platform FROM video_games WHERE Platform IS NOT NULL
+    ) vg2
+    CROSS JOIN (
+        SELECT DISTINCT Year_of_Release FROM video_games WHERE Year_of_Release IS NOT NULL
+    ) vg3
 )
-select ac.Genre, ac.Platform, ac.Year_of_Release, 
-isnull(sum (vg.Global_Sales), 0) globalSales
-from allCross_CTE ac left join video_games vg
-on ac.Genre= vg.Genre and ac.Platform= vg.Platform
-and ac.Year_of_Release= vg.Year_of_Release
-group by ac.Genre, ac.Platform, ac.Year_of_Release
-order by ac.Genre, ac.Platform, ac.Year_of_Release
+SELECT ac.Genre, ac.Platform, ac.Year_of_Release, 
+       ISNULL(SUM(vg.Global_Sales), 0) AS globalSales
+FROM allCross_CTE ac
+LEFT JOIN video_games vg
+    ON ac.Genre = vg.Genre 
+   AND ac.Platform = vg.Platform
+   AND ac.Year_of_Release = vg.Year_of_Release
+GROUP BY ac.Genre, ac.Platform, ac.Year_of_Release
+ORDER BY ac.Genre, ac.Platform, ac.Year_of_Release;
 
-
-
---QUESTION 4:
-with allYears_CTE as
-(
-select distinct vg1.Platform, vg2.Year_of_Release
-from video_games vg1 cross join video_games vg2
-where vg2.Year_of_Release is not null and vg1.Platform is not null and
-vg1.Year_of_Release !=2020
+-- QUESTION 4:
+WITH allYears_CTE AS (
+    SELECT DISTINCT vg1.Platform, vg2.Year_of_Release
+    FROM video_games vg1
+    CROSS JOIN video_games vg2
+    WHERE vg2.Year_of_Release IS NOT NULL 
+      AND vg1.Platform IS NOT NULL 
+      AND vg1.Year_of_Release != 2020
 ),
-globalSale_CTE as
-(
-select ay.Platform, ay.Year_of_Release, isnull(sum (vg3.Global_Sales), 0) Global_Sales
-from allYears_CTE ay left join video_games vg3 on
-ay.Platform= vg3.Platform and ay.Year_of_Release= vg3.Year_of_Release
-group by ay.Platform, ay.Year_of_Release
+globalSale_CTE AS (
+    SELECT ay.Platform, ay.Year_of_Release, ISNULL(SUM(vg3.Global_Sales), 0) AS Global_Sales
+    FROM allYears_CTE ay
+    LEFT JOIN video_games vg3 
+        ON ay.Platform = vg3.Platform 
+       AND ay.Year_of_Release = vg3.Year_of_Release
+    GROUP BY ay.Platform, ay.Year_of_Release
 ),
-LagGlobalSale_cte as
-(
-select *,
-lag (Global_Sales) over (partition by platform order by year_of_release) LagGlobalSale
-from globalSale_CTE
+LagGlobalSale_CTE AS (
+    SELECT *,
+           LAG(Global_Sales) OVER (PARTITION BY Platform ORDER BY Year_of_Release) AS LagGlobalSale
+    FROM globalSale_CTE
 ),
-YoY_CTE as
-(
-select *, 
-rank() over (partition by Platform order by YoY desc) rn
-from (select Platform, Year_of_Release,
-case
-when LagGlobalSale is not null and LagGlobalSale!=0 
-then (Global_Sales- LagGlobalSale)/LagGlobalSale *100
-end 'YoY'
-from LagGlobalSale_cte) tbl1
+YoY_CTE AS (
+    SELECT *, 
+           RANK() OVER (PARTITION BY Platform ORDER BY YoY DESC) AS rn
+    FROM (
+        SELECT Platform, Year_of_Release,
+               CASE
+                   WHEN LagGlobalSale IS NOT NULL AND LagGlobalSale != 0 
+                   THEN (Global_Sales - LagGlobalSale) / LagGlobalSale * 100
+               END AS YoY
+        FROM LagGlobalSale_CTE
+    ) tbl1
 )
-select top 1 Platform, Year_of_Release
-from YoY_CTE
-where rn= 1
-order by YoY desc
+SELECT TOP 1 Platform, Year_of_Release
+FROM YoY_CTE
+WHERE rn = 1
+ORDER BY YoY DESC;
 
 
 
